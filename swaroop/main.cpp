e3972f66e0ad1d6c6a49638c6f575f7f64da83db
Ashok Gadhveer <apple@Apples-MacBook-Pro.local>
2014-09-09 22:11:21 +0530

    Initial Commit

---
+++ swaroop/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  swaroop
+//
+//  Created by Ashok Gadhveer on 09/09/14.
+//  Copyright (c) 2014 MYAPPP. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
